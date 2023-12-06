49e9a842eed9727ed3705c5f85f77e0a6ac6e1ed
JefersonCopini <116360705+JefersonCopini@users.noreply.github.com>
2023-12-06 01:37:31 +0000

    fix:Final

--- app/components/Aside.tsx
+++ app/components/Aside.tsx
@@ -1,8 +1,8 @@
 export default function Aside() {
     return (
         <div className="w-1/6">
-            <a href="/admin/Car">Listar Carros</a> <br/>
-            <a href="/admin/Car/new">Cadastrar Carros</a>
+            <a href="/admin/Car">Listar Carros</a> <br></br>
+            <a href="/admin/Car/new">Cadastrar Carros </a>
 
         </div>
     )
