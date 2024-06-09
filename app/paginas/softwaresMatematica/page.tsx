e27db4a117ffbb2128231de4d2ba87884e8fc6f6
JefersonCopini <116360705+JefersonCopini@users.noreply.github.com>
2024-06-09 19:00:30 +0000

    blackgroundcolor edit

--- app/paginas/softwaresMatematica/page.tsx
+++ app/paginas/softwaresMatematica/page.tsx
@@ -12,7 +12,7 @@ export default async function Matematica() {
     }
 
     return (
-        <main className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+        <main className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 bg-[#4d4d4d]">
             <div className="md:col-span-2 lg:col-span-3 mt-4 text-white text-center">
                 <h2 id="matematica">
                     Conheça os softwares educacionais voltados a matemática
